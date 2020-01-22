8757953eae11b9431ecc561d3c9ff8016388e828
aoakua <aoakua@naver.com>
2020-01-22 15:49:42 +0900

    folio

--- folio/js/folio.js
+++ folio/js/folio.js
@@ -6,13 +6,19 @@ logo.addEventListener("click", function(){
 });
 
 function deviceCheck(){
-    let pcDevice = "win16|win32|win64|mac|macintel";
-    let thisDevice = navigator.platform;
+    const pcDevice = "win16|win32|win64|mac|macintel";
+    const thisDevice = navigator.platform;
     let workList = document.querySelector("#main .work .work_list");
 
     if(thisDevice){
         if(pcDevice.indexOf(navigator.platform.toLowerCase()) < 0){
             //mobile
+            workList.classList.remove("effect");
+        }else if(navigator.userAgent.indexOf("Android")>0 || 
+        navigator.userAgent.indexOf("iPhone") > 0|| 
+        navigator.userAgent.indexOf("iPod") > 0|| 
+        navigator.userAgent.indexOf("BlackBerry") > 0){
+            //tablet
             workList.classList.remove("effect");
         }else{
             //pc
