534a11c043cfdb68aad8d9036252dd239d8548b9
aoakua <aoakua@naver.com>
2020-01-30 13:33:25 +0900

    folio

--- folio/js/folio.js
+++ folio/js/folio.js
@@ -2,7 +2,7 @@
 function deviceCheck(){
     const pcDevice   = "win16|win32|win64|mac|macintel",
           thisDevice = navigator.platform;
-    let workList = document.querySelector("#main .work .work_list");
+    var workList = document.querySelector("#main .work .work_list");
 
     if(thisDevice){
         if(pcDevice.indexOf(navigator.platform.toLowerCase()) < 0){
@@ -18,7 +18,7 @@ function deviceCheck(){
 }
 
 function init(){
-    let logo = document.querySelector(".logo"),
+    var logo = document.querySelector(".logo"),
         gnb  = document.querySelector(".gnb li a");
 
     logo.addEventListener("click", function(){
