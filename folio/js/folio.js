8413c996028e4d94c84e8eabdabbd8867f3acae2
aoakua <aoakua@naver.com>
2020-02-22 14:38:03 +0900

    #020222 folio 대수정

--- folio/js/folio.js
+++ folio/js/folio.js
@@ -1,4 +1,62 @@
 
+$(function() {
+    var chartCont = function(){
+        $('.chart1').easyPieChart({
+            scaleColor: "#ecf0f1",
+            lineWidth: 20,
+            lineCap: 'butt',
+            barColor: '#9d64c1',
+            trackColor:	"#ecf0f1",
+            size: 200,
+            animate: 1000,
+            scaleLength: 0
+        });
+        $('.chart2').easyPieChart({
+            scaleColor: "#ecf0f1",
+            lineWidth: 20,
+            lineCap: 'butt',
+            barColor: '#4e66a7',
+            trackColor:	"#ecf0f1",
+            size: 200,
+            animate: 1000,
+            scaleLength: 0
+        });
+        $('.chart3').easyPieChart({
+            scaleColor: "#ecf0f1",
+            lineWidth: 20,
+            lineCap: 'butt',
+            barColor: '#4ea78e',
+            trackColor:	"#ecf0f1",
+            size: 200,
+            animate: 1000,
+            scaleLength: 0
+        });
+        $('.chart4').easyPieChart({
+            scaleColor: "#ecf0f1",
+            lineWidth: 20,
+            lineCap: 'butt',
+            barColor: '#e8c496',
+            trackColor:	"#ecf0f1",
+            size: 200,
+            animate: 1000,
+            scaleLength: 0
+        });
+    }
+
+    
+    $(window).scroll(function(){
+        var skill = $('#main .about .about_box .skill');
+
+        if($(this).scrollTop() > skill.offset().top -650){
+            chartCont();
+        }
+    });
+    
+    
+});
+
+
+
 function deviceCheck(){
     const pcDevice   = "win16|win32|win64|mac|macintel",
           thisDevice = navigator.platform;
@@ -17,6 +75,7 @@ function deviceCheck(){
     }
 }
 
+
 function init(){
     var logo = document.querySelector(".logo"),
         gnb  = document.querySelector(".gnb li a");
@@ -24,7 +83,6 @@ function init(){
     logo.addEventListener("click", function(){
         location.reload();
     });
-
     deviceCheck();
 }
 init();
