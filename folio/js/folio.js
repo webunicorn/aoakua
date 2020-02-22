70b62c6ba4ca6371096ee555e04d985a10815086
aoakua <aoakua@naver.com>
2020-02-22 14:59:47 +0900

    200222 folio

--- folio/js/folio.js
+++ folio/js/folio.js
@@ -56,33 +56,3 @@ $(function() {
 });
 
 
-
-function deviceCheck(){
-    const pcDevice   = "win16|win32|win64|mac|macintel",
-          thisDevice = navigator.platform;
-    var workList = document.querySelector("#main .work .work_list");
-
-    if(thisDevice){
-        if(pcDevice.indexOf(navigator.platform.toLowerCase()) < 0){
-            //mobile
-            workList.classList.remove("effect");
-        }else if(navigator.userAgent.match(/iPad/i)||navigator.userAgent.match(/Tablet/i)||navigator.userAgent.match(/Android/i)||navigator.userAgent.match(/iPhone|iPod/i) ){
-            //tablet
-            workList.classList.remove("effect");
-        }else{
-            //pc
-        }
-    }
-}
-
-
-function init(){
-    var logo = document.querySelector(".logo"),
-        gnb  = document.querySelector(".gnb li a");
-
-    logo.addEventListener("click", function(){
-        location.reload();
-    });
-    deviceCheck();
-}
-init();
